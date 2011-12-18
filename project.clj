a31c0fae3ec5f020c4d4ec0594f5f6b7332ac391
Jiri Marsik <jiri.marsik89@gmail.com>
2011-12-18 17:13:07 +0800

    Initial commit

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject esa-wordseg "1.0.0-SNAPSHOT"
+  :description "An implementation of the ESA unsupervised word segmentation algorithm."
+  :dependencies [[org.clojure/clojure "1.3.0"]])
---
+++ test/esa_wordseg/test/core.clj
@@ -0,0 +1,6 @@
+(ns esa-wordseg.test.core
+  (:use [esa-wordseg.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
