f640f39df7505e5701d45d3b7bbeda7489136e94
Jiri Marsik <jiri.marsik89@gmail.com>
2011-12-30 14:36:35 +0800

    Some tests I tend to run a lot

--- src/esa_wordseg/core.clj
+++ src/esa_wordseg/core.clj
@@ -1,1 +1,28 @@
-(ns esa-wordseg.core)
+(ns esa-wordseg.core
+  (:use (esa-wordseg trie preproc eval)))
+
+(def test-file "D:\\Dev\\esa-wordseg\\icwb2-data\\testing\\cityu_test.utf8")
+
+(def test-data (slurp test-file))
+
+(def test-charvecs (map vec (split-on-newline [cityu-data])))
+
+(def test-alphabet-size (get-alphabet-size test-charvecs))
+
+(def test-limit 10)
+
+(def test-freq-trie (get-frequency-trie test-charvecs test-limit))
+
+(def test-smooth-param 0.000001)
+
+(def test-tries (add-entropy-tries test-freq-trie test-alphabet-size test-smooth-param))
+
+(def test-averages (get-average-statistics test-tries))
+
+(def test-stats {:tries test-tries, :averages test-averages})
+
+(def test-exp 1)
+
+(def the-word (subvec (nth test-charvecs 2) 0 2))
+
+(def the-following-word (subvec (nth test-charvecs 2) 2 4))
